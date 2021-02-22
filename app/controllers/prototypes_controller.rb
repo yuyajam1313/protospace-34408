6f29ee9febf92e896260a6268bad970cbe3967bb
yuyajam1313 <78283195+yuyajam1313@users.noreply.github.com>
2021-02-22 15:44:44 +0900

    修正依頼

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,9 +30,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to prototype_path(prototype)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
     else
       render :edit
     end
